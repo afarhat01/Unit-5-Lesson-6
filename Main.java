98217b8e036cf4d454a7da0b66f830a0118ddbe4
Student: Alex Alex <94848745+afarhat01@users.noreply.github.com>
2022-01-12 14:27:55 +0000

    Increase Size

--- Main.java
+++ Main.java
@@ -10,7 +10,6 @@ class Main {
         Book b = new Book("I am Malala", "Malala Yousafzai", 2013, 288, true);
         Book b1 = new Book("I am Malala", "bob", 20123, 2828, false);
         
-        
         System.out.println(b);
 
         Book g = new Book("The Godfather", "Mario Puzo", 2002, 448, false);
@@ -33,9 +32,31 @@ class Main {
       System.out.println("Are the books equal? " + b.equals(g));
       System.out.println("Are the books equal? " + b.equals(b1));
 
+      Book def1 = new Book();
+      Book def2 = new Book();
+      System.out.println("This is Default Book 1 \n" + def1);
+      System.out.println("This is Default Book 2 \n" + def2);
+      System.out.println("Are the default books equal? " + def1.equals(def2));
 
-
-
+			//Create Library
+			Library lib = new Library();
+			System.out.println(lib);
+			//add books to our Library
+			lib.addBooks(b);
+			lib.addBooks(b1);
+			lib.addBooks(g);
+			lib.addBooks(def1);
+			lib.addBooks(def2);
+			lib.addBooks(b);
+			lib.addBooks(b1);
+			lib.addBooks(g);
+			lib.addBooks(def1);
+			lib.addBooks(def2);
+			System.out.println(lib);
+			System.out.println("Increasing Size of Array");
+			lib.increaseSize();
+			lib.addBooks(b);
+			System.out.println(lib);
 
   }
 }
